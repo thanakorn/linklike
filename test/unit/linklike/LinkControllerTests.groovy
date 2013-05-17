5de4fb5a2e0b76b569037a9df90cf3cbf5d15162
Thanakorn <stylus_l@hotmail.com>
2013-05-17 21:18:42 +0700

    TestCreateLink in LinkController

--- test/unit/linklike/LinkControllerTests.groovy
+++ test/unit/linklike/LinkControllerTests.groovy
@@ -4,14 +4,23 @@ package linklike
 
 import grails.test.mixin.*
 import org.junit.*
+import grails.test.mixin.domain.DomainClassUnitTestMixin
 
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(LinkController)
+@TestMixin(DomainClassUnitTestMixin)
+@Mock([Link])
 class LinkControllerTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+    void testCreateLink(){
+		params.title = "Create"
+		params.url = "www.google.com"
+		
+		controller.create()
+		
+		assert Link.count == 1
+	}
+	
 }
